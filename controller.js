eb1c8e6be2511b5dc9bcbd11111483f0b5e5abd8
Masood Mawlavizada <masmawlav@ucdavis.edu>
2017-06-27 21:03:38 -0700

    working post to database

--- controller.js
+++ controller.js
@@ -1,6 +1,29 @@
 (function(){
 	var app = angular.module('mainModule',['ngMaterial', 'ngMessages']);
-	app.controller('FormCtrl', function(){
+	app.controller('FormCtrl', function($scope, $http){
+		$http.get('/stereobasecontacts').then(successCallBack,errorCallBack);
+		function successCallBack(response) {
+			// console.log("I got data");
+			// console.log(response);
+		}
+		function errorCallBack(error) {
+			// console.log("We got an error");
+		}
 
+		$scope.addContact = function() {
+			// console.log($scope.user);
+			$http.post('stereobasecontacts', $scope.user).then(postSuccess, postError);
+			function postSuccess(response) {
+				console.log(response);
+			}
+			function postError(error){
+				// console.log(error);
+				// console.log("we have an error");
+			}
+			// console.log("Submit works");
+			// $http.post('stereobasecontacts', $scope.user)
+			// console.log($scope.user);
+		};
+		// console.log($scope.user);
 	});
 })();
--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('stereobasecontacts', ['stereobasecontacts']);
+var bodyParser = require('body-parser');
+
 
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+app.get('/stereobasecontacts', function(req, res){
+	console.log("I have recieved a GET request");
+	db.stereobasecontacts.find(function(err, docs){
+		// console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/stereobasecontacts', function(req,res){
+	console.log(req.body);
+	db.stereobasecontacts.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
 
 
 app.listen(3000);
